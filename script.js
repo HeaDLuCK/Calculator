b1bf2ea3787c5aa78ad553e9112f9b909df07e28
HeaDLuCK <106028299+HeaDLuCK@users.noreply.github.com>
2022-12-24 23:09:58 +0100

    Add EventListener to Buttons
    we fixd buttons so they're workng well and merge them with function
    we create for 4 operation add multi divide subtract

---
+++ script.js
@@ -0,0 +1,117 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+};
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+};
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+};
+
+function divide(firstNumber, secondNumber) {
+    if (secondNumber === 0) {
+        console.log('you cant divide with 0');
+        return 0
+    } else {
+        return firstNumber / secondNumber;
+
+    }
+};
+
+function operation(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case 'x':
+            return multiply(firstNumber, secondNumber);
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '%':
+            return divide(firstNumber, secondNumber);
+    }
+};
+
+
+
+
+
+let numbers = document.querySelectorAll('.numberBtn')
+let i = 1;
+let operationInfo = {};
+let output = document.querySelector('.outputResult')
+let outputText = document.querySelector('.outputText')
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if (!(operationInfo['operator'])) {
+            if (!(operationInfo[`num1`])) {
+                operationInfo[`num1`] = number.textContent;
+                output.textContent = operationInfo[`num1`]
+            } else {
+                if (operationInfo['finish']) {
+                    operationInfo[`num1`] = number.textContent;
+                    output.textContent = operationInfo[`num1`];
+                    delete operationInfo['finish'];
+                } else {
+                    operationInfo[`num1`] += number.textContent;
+                    output.textContent = operationInfo[`num1`]
+                }
+
+
+            }
+
+        } else {
+            if (!(operationInfo[`num2`])) {
+                operationInfo[`num2`] = number.textContent;
+                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+            } else {
+                operationInfo[`num2`] += number.textContent;
+                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+            }
+        }
+
+        console.log(operationInfo);
+    })
+})
+
+let operators = document.querySelectorAll('.operatorBtn')
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        if ((operationInfo[`num1`]) && !(operationInfo[`num2`])) {
+            operationInfo['operator'] = operator.innerHTML
+            output.textContent = operationInfo[`num1`] + operationInfo[`operator`]
+            delete operationInfo['finish'];
+
+        } else if ((operationInfo[`num1`]) && (operationInfo[`num2`])) {
+            let result = operation(+operationInfo['num1'], +operationInfo['num2'], operationInfo['operator'])
+            console.log(result);
+            outputText.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+            operationInfo[`num1`] = result;
+            operationInfo['operator'] = operator.innerHTML;
+            delete operationInfo['num2'];
+            output.textContent = result
+
+        }
+        console.log(operationInfo);
+    })
+})
+
+let equal = document.querySelector('.equal')
+equal.addEventListener('click', () => {
+    let result = operation(+operationInfo['num1'], +operationInfo['num2'], operationInfo['operator']);
+    operationInfo['finish'] = true;
+    outputText.textContent=operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+    output.textContent = result;
+    operationInfo['num1'] = result;
+    delete operationInfo['num2'];
+    delete operationInfo['operator'];
+    console.log(result);
+})
+
+let clear = document.querySelector('.clear')
+clear.addEventListener('click', () => {
+    operationInfo = {}
+    output.textContent = 0
+    outputText.textContent=''
+})
