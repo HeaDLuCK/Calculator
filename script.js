f2c81325c93f7d836df6ecd4d91b9fddd51a01d8
HeaDLuCK <106028299+HeaDLuCK@users.noreply.github.com>
2022-12-25 01:59:15 +0100

    Add keyboard support
    we fixed some bug of back space the second number will give 0
    we switch it to none 'delete number'
    
    we change some arrow function to normal function so we can use them
    again in keyboard support functions

--- script.js
+++ script.js
@@ -23,19 +23,80 @@ function divide(firstNumber, secondNumber) {
 function operation(firstNumber, secondNumber, operator) {
     switch (operator) {
         case 'x':
+        case '*':
             return multiply(firstNumber, secondNumber);
         case '+':
             return add(firstNumber, secondNumber);
         case '-':
             return subtract(firstNumber, secondNumber);
         case '%':
+        case '/':
             return divide(firstNumber, secondNumber);
     }
 };
 
+function resultOfOperator() {
+    if (operationInfo[`num1`] && operationInfo['num2'] && operationInfo['operator']) {
+        let result = operation(+operationInfo['num1'], +operationInfo['num2'], operationInfo['operator']);
+        operationInfo['finish'] = true;
+        outputText.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+        output.textContent = result;
+        operationInfo['num1'] = result;
+        delete operationInfo['num2'];
+        delete operationInfo['operator'];
+        console.log(result);
+    }
+}
 
+function backSpaceNumber() {
+    if (!(operationInfo['operator'])) {
+        if (operationInfo[`num1`]) {
+            if (!(operationInfo['finish'])) {
+                let newNumber = operationInfo[`num1`].slice(0, -1)//second parameter is not included in the output
+                if (newNumber !== '') {
+                    operationInfo[`num1`] = newNumber
+                    output.textContent = operationInfo[`num1`]
+                } else {
+                    operationInfo[`num1`] = 0
+                    output.textContent = operationInfo[`num1`]
+                }
+            }
+        }
 
+    } else {
+        if ((operationInfo[`num2`])) {
+            let newNumber = operationInfo[`num2`].slice(0, -1)//second parameter is not included in the output
+            if (newNumber !== '') {
+                operationInfo[`num2`] = newNumber
+                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+            } else {
+                delete operationInfo[`num2`]
+                output.textContent = operationInfo[`num1`] + operationInfo[`operator`]
+            }
 
+        }
+    }
+}
+
+function dotCheckInNumbers() {
+    if (!(operationInfo['operator'])) {
+        if (!dotCheck) {
+            if (operationInfo[`num1`]) {
+                operationInfo[`num1`] += dot.textContent;
+                output.textContent = operationInfo[`num1`]
+                dotCheck = true
+            }
+        }
+    } else {
+        if (!dotCheck) {
+            if (operationInfo[`num2`]) {
+                operationInfo[`num2`] += dot.textContent;
+                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+                dotCheck = true
+            }
+        }
+    }
+}
 
 let numbers = document.querySelectorAll('.numberBtn')
 let dotCheck = false;
@@ -99,18 +160,10 @@ operators.forEach(operator => {
     })
 })
 
+
 let equal = document.querySelector('.equal')
 equal.addEventListener('click', () => {
-    if (operationInfo[`num1`] && operationInfo['num2'] && operationInfo['operator']) {
-        let result = operation(+operationInfo['num1'], +operationInfo['num2'], operationInfo['operator']);
-        operationInfo['finish'] = true;
-        outputText.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
-        output.textContent = result;
-        operationInfo['num1'] = result;
-        delete operationInfo['num2'];
-        delete operationInfo['operator'];
-        console.log(result);
-    }
+    resultOfOperator()
 })
 
 let clear = document.querySelector('.clear')
@@ -147,53 +200,71 @@ change.addEventListener('click', () => {
 
 let dot = document.querySelector('.dot')
 dot.addEventListener('click', () => {
-    if (!(operationInfo['operator'])) {
-        if (!dotCheck) {
-            if (operationInfo[`num1`]) {
-                operationInfo[`num1`] += dot.textContent;
-                output.textContent = operationInfo[`num1`]
-                dotCheck = true
-            }
-        }
-    } else {
-        if (!dotCheck) {
-            if (operationInfo[`num2`]) {
-                operationInfo[`num2`] += dot.textContent;
-                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
-                dotCheck = true
-            }
-        }
-    }
+    dotCheckInNumbers()
 })
+
 
 let shift = document.querySelector('.shift')
 shift.addEventListener('click', () => {
-    if (!(operationInfo['operator'])) {
-        if (operationInfo[`num1`]) {
-            if (!(operationInfo['finish'])) {
-                let newNumber = operationInfo[`num1`].slice(0, -1)//second parameter is not included in the output
-                if (newNumber !== '') {
-                    operationInfo[`num1`] = newNumber
-                    output.textContent = operationInfo[`num1`]
-                } else {
-                    operationInfo[`num1`] = 0
-                    output.textContent = operationInfo[`num1`]
+    backSpaceNumber()
+})
+
+
+window.addEventListener('keydown', (e) => {
+    if (isNaN(+e.key)) {
+        let verification = ['*', '-', '/', '+', 'Backspace', 'Enter', '.']
+        if (verification.includes(e.key)) {
+            if (e.key !== 'Enter' && e.key !== 'Backspace' && e.key !== '.') {
+                if ((operationInfo[`num1`]) && !(operationInfo[`num2`])) {
+                    operationInfo['operator'] = e.key
+                    output.textContent = operationInfo[`num1`] + operationInfo[`operator`]
+                    dotCheck = false
+                    delete operationInfo['finish'];
+
+                } else if ((operationInfo[`num1`]) && (operationInfo[`num2`])) {
+                    let result = operation(+operationInfo['num1'], +operationInfo['num2'], operationInfo['operator'])
+                    console.log(result);
+                    outputText.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+                    operationInfo[`num1`] = result;
+                    operationInfo['operator'] = e.key
+                    delete operationInfo['num2'];
+                    output.textContent = result
+                    dotCheck = false
+
                 }
+            } else if (e.key === 'Backspace') {
+                backSpaceNumber()
+            } else if (e.key === 'Enter') {
+                resultOfOperator()
+            } else if (e.key === '.') {
+                dotCheckInNumbers()
             }
         }
 
     } else {
-        if ((operationInfo[`num2`])) {
-            let newNumber = operationInfo[`num2`].slice(0, -1)//second parameter is not included in the output
-            if (newNumber !== '') {
-                operationInfo[`num2`] = newNumber
-                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+        if (!(operationInfo['operator'])) {
+            if (!(operationInfo[`num1`])) {
+                operationInfo[`num1`] = e.key;
+                output.textContent = operationInfo[`num1`]
             } else {
-                operationInfo[`num2`] = 0
-                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+                if (operationInfo['finish']) {
+                    operationInfo[`num1`] = e.key;
+                    output.textContent = operationInfo[`num1`];
+                    delete operationInfo['finish'];
+                } else {
+                    operationInfo[`num1`] += e.key;
+                    output.textContent = operationInfo[`num1`]
+                }
             }
 
+        } else {
+            if (!(operationInfo[`num2`])) {
+                operationInfo[`num2`] = e.key;
+                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+            } else {
+                operationInfo[`num2`] += e.key;
+                output.textContent = operationInfo[`num1`] + operationInfo[`operator`] + operationInfo[`num2`]
+            }
         }
     }
-
 })
